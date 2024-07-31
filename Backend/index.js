368f794c5e102d80520e8ed4cef89780f5f6e333
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-07-31 12:22:59 +0530

    Models Created

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 import dbConnection from "./utils/db.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 const port = process.env.PORT || 8005;
 
 app.listen(port, () => {
