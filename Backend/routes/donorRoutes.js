f10cca5f784c860211a0b3eb53853d6dd53632b5
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-07-31 16:53:09 +0530

    Added controllers and routes for prospect. And added middleware

--- Backend/routes/donorRoutes.js
+++ Backend/routes/donorRoutes.js
@@ -1,12 +1,14 @@
 import express from "express"
 import donorController from "../controllers/donorController.js"
+import { verifyAuthorization } from "../middlewares/verification.js"
 
 const router = express.Router()
 
-router.post("/createDonor", donorController.createDonor)
+router.post("/createDonor", verifyAuthorization, donorController.createDonor)
 
 router.get("/allDonors", donorController.getAllDonors)
 router.get("/getDonor/:id", donorController.getOneDonor)
+router.get("/stats", donorController.donorStatistics)
 
 router.put("/updateDonor/:id", donorController.updateDonor)
 
