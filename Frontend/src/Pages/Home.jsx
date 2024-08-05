5309445677c4be570aed2381378b231f5196221d
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-08-05 16:49:20 +0530

    Accidentally Deleted the folders in Local and Retrived Successfully

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import Hero from '../Components/Hero'
+import Featured from '../Components/Featured'
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <Featured />
+        </div>
+    )
+}
+
+export default Home
