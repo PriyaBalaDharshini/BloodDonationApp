533f3b27e9ba5bd653b5c46720179fbbee91077e
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-08-05 16:26:43 +0530

    Basic componets and Pages have been created

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from '../Components/NavBar'
+import Hero from '../Components/Hero'
+import Featured from '../Components/Featured'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <NavBar />
+            <Hero />
+            <Featured />
+        </div>
     )
 }
 
